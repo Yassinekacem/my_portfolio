84b7a259e820d74b696995e5fd170595ba45bc10
yassine kacem <110304629+Yassinekacem@users.noreply.github.com>
2023-07-21 15:28:39 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,69 +1,36 @@
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
-import SlideUp from "./SlideUp"
-import { BsGithub } from "react-icons/bs"
+"use client" // this is a client component
 
-const projects = [
-  {
-    name: "Web site : Scooby ",
-    description:
-      "This web application is part of my final year project. It is dedicated to pet enthusiasts in Tunisia. Technologies used: Next.js, Node.js, Express, Prisma, PostgreSQL, Bootstrap, TypeScript.",
-    image: "/thankfulthoughts.png",
-    github: "https://github.com/Yassinekacem?tab=repositories",
-  },
- 
-]
-
-const ProjectsSection = () => {
-  return (
-    <section id="projects">
-      <h1 className="my-10 text-center font-bold text-4xl">
-        Projects
-        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
-      </h1>
-
-      <div className="flex flex-col space-y-28">
-        {projects.map((project, idx) => {
-          return (
-            <div key={idx}>
-              <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
-                  <div className=" md:w-1/2">
-                    <Link href="">
-                      <Image
-                        src={project.image}
-                        alt=""
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
-                      />
-                    </Link>
-                  </div>
-                  <div className="mt-8 md:w-1/2">
-                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
-                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                      {project.description}
-                    </p>
-                    <div className="flex flex-row align-bottom space-x-4">
-                      <Link href={project.github} target="_blank">
-                        <BsGithub
-                          size={30}
-                          className="hover:-translate-y-1 transition-transform cursor-pointer"
-                        />
-                      </Link>
-                    
-                    </div>
-                  </div>
-                </div>
-              </SlideUp>
-            </div>
-          )
-        })}
-        
-      </div>
-    </section>
-  )
+import React, { useEffect, useRef, ReactNode } from "react"
+interface Props {
+  offset?: string
+  children?: ReactNode
+  // any props that come into the component
 }
 
-export default ProjectsSection
+export default function SlideUp({ children, offset = "0px" }: Props) {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.remove("opacity-0")
+            entry.target.classList.add("animate-slideUpCubiBezier")
+          }
+        })
+      },
+      { rootMargin: offset }
+    )
+
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+  }, [ref])
+
+  return (
+    <div ref={ref} className="relative opacity-0">
+      {children}
+    </div>
+  )
+}
